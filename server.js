536f3f25064fb53e908d7ca8404fbcdea12a3ca5
Robert Shepley <shepleysound@gmail.com>
2022-09-05 14:42:08 -0700

    Begin auth0 implementation.

--- server.js
+++ server.js
@@ -2,10 +2,31 @@
 
 const express = require('express');
 const app = express();
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
+const port = process.env.PORT || 3002;
 
-const port = process.env.PORT || 3002
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 app.get('/', (req, res) => {
   res.send('Hello Coin Fellows')
 })
+
+app.get('/authorized', (req, res) => {
+  res.send('Secured Resource');
+})
+
+app.listen(port);
