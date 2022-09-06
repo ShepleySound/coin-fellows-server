85754e7e39fe90f2ed89e1e2e1675f6eba70e963
Robert Shepley <shepleysound@gmail.com>
2022-09-06 16:14:32 -0700

    Adds unique users to database.

--- server.js
+++ server.js
@@ -7,14 +7,20 @@ const jwtCheck = require('./middleware/jwt-check');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+// Bring in the User model
+const User = require('./models/user.js');
 
 // Connect Mongoose to MongoDB
 mongoose.connect(process.env.MONGODB_CONNECTION);
 
+// Open connection to MongoDB using Mongoose.
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => console.log('Mongoose is now connected.'));
+
+// Create Express server.
 const app = express();
 const port = process.env.PORT || 3002;
-
-
 
 app.use(cors());
 app.use(express.json());
@@ -23,10 +29,21 @@ app.get('/', (req, res) => {
   res.send('Hello Coin Fellows')
 })
 
-app.get('/protected', jwtCheck, (req, res) => {
-  console.log(req)
-  res.status(200).send('hello protected!!!!')
-})
+app.get('/protected', jwtCheck, getUser)
+
+async function getUser(req, res, next) {
+  try {
+  console.log('REQ', req.auth.sub)
+  const userDoc = await User.findOneAndUpdate(
+    { _id: req.auth.sub },
+    { name: req.auth.sub },
+    { upsert: true, new: true }
+  )
+  res.status(200).send(userDoc)
+  } catch (error) {
+    next(error);
+  }
+}
 
 app.get('/authorized', (req, res) => {
   res.send('Secured Resource');
